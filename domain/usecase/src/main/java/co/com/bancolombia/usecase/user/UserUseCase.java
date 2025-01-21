fd62fd638cba31228fa8b0a5ba5177e771ae78ca
unknown <dcfajardo@gmail.com>
2025-01-21 18:28:01 -0500

    mensaje

---
+++ domain/usecase/src/main/java/co/com/bancolombia/usecase/user/UserUseCase.java
@@ -0,0 +1,10 @@
+package co.com.bancolombia.usecase.user;
+
+import lombok.RequiredArgsConstructor;
+@RequiredArgsConstructor
+public class UserUseCase {
+
+    public String user(){
+        return "<<<Este es el usuario Consumidor>>>>";
+    }
+}
